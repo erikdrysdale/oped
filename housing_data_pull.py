8209c8936d578a280353a2abbce07e925963e6ab
erikdrysdale <13ewd@queensu.ca>
2020-09-15 23:54:55 -0400

    Add files via upload

---
+++ housing_data_pull.py
@@ -0,0 +1,45 @@
+"""
+PULL HOUSING START, POPULATION DATA, AND HOUSE PRICES
+
+https://www.sfu.ca/mpp/faculty-associates/josh-gordon/selected-publications.html
+https://www.theglobeandmail.com/opinion/article-the-supply-crisis-in-canadas-housing-market-isnt-backed-up-by-the/
+
+"""
+
+import os
+import pandas as pd
+import numpy as np
+
+# Calculate the population adjusted housing starts
+
+# Load population
+tmp1 = pd.read_csv('pop_2000.csv', nrows=47, header=None)
+tmp1 = pd.DataFrame(tmp1.iloc[:,1:].T.values,columns=tmp1.iloc[:,0])
+tmp2 = pd.read_csv('pop_2001.csv', nrows=47, header=None)
+tmp2 = pd.DataFrame(tmp2.iloc[:,1:].T.values,columns=tmp2.iloc[:,0])
+tmp1.columns = [z[0] for z in tmp1.columns.str.replace('[^A-Za-z\\,\\s\\-]','').str.split('\\,\\s')]
+tmp2.columns = [z[0] for z in tmp2.columns.str.replace('[^A-Za-z\\,\\s\\-]','').str.split('\\,\\s')]
+assert tmp1.columns.isin(tmp2.columns).all()
+tmp1.Date = pd.to_datetime(tmp1.Date,format='%b-%y')
+tmp2.Date = pd.to_datetime(tmp2.Date,format='%y-%b')
+df_pop = pd.concat([tmp1, tmp2]).melt('Date',None,'geo')
+df_pop.value = df_pop.value.str.replace('\\,','').astype(float)
+df_pop = df_pop[df_pop.geo.isin(['Vancouver','Toronto'])].reset_index(None,True)
+df_pop = df_pop.assign(year=lambda x: x.Date.dt.year).groupby(['geo','year']).value.mean().reset_index()
+
+# Load housing starts
+tmp1 = pd.read_csv('starts_toronto.csv',nrows=360).assign(geo='Toronto')
+tmp2 = pd.read_csv('starts_vancouver.csv',nrows=360).assign(geo='Vancouver')
+df_starts = pd.concat([tmp1, tmp2])
+df_starts = df_starts[['date','All','geo']].rename(columns={'All':'starts'})
+df_starts.starts = df_starts.starts.str.replace(',','').astype(int)
+dd = np.where(df_starts.date.str.contains('^[A-Z]'),df_starts.date, df_starts.date.str.split('-',1,True).iloc[:,[1,0]].apply(lambda x: '-'.join(x),1))
+df_starts.date = pd.to_datetime(dd, format='%b-%y')
+df_starts = df_starts.assign(year=lambda x: x.date.dt.year).groupby(['geo','year']).starts.sum().reset_index()
+
+# Merge
+df_both = df_pop.merge(df_starts).assign(rel = lambda x: x.starts / x.value)
+df_both = df_both.melt(['geo','year'],['starts','rel'],'metric','val')
+df_res = df_both.assign(half=lambda x: np.where(x.year <= 2009,'<2010','>2010')).groupby(['geo','metric','half']).val.mean().reset_index()
+df_res = df_res.pivot_table('val',['geo','metric'],'half').reset_index()
+print(df_res.assign(ratio=lambda x: x['>2010']/x['<2010']-1))
